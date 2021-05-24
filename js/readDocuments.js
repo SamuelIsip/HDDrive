6879828d983f0f752f12881b0639a5cac8f8d19f
Samuel Isip <samuel.isip62@gmail.com>
2021-05-24 23:43:50 +0200

    Files adjust

--- js/readDocuments.js
+++ js/readDocuments.js
@@ -22,59 +22,64 @@ function readDocuments() {
   xhttp.send(); */
 
   function createList(doc, div_list, table) {
+    console.log(doc);
     var count = 0;
+    //Head
+    let thead = document.createElement("thead");
+    let tr = document.createElement("tr");
+    let th1 = document.createElement("th");
+    let th2 = document.createElement("th");
+    let th3 = document.createElement("th");
+
+    th1.appendChild(document.createTextNode("Name"));
+    th2.appendChild(document.createTextNode("File Size"));
+    th3.appendChild(document.createTextNode("Date"));
+
+    tr.appendChild(th1);
+    tr.appendChild(th2);
+    tr.appendChild(th3);
+
+    thead.appendChild(tr);
+
+    var tbody = document.createElement("tbody");
+
     doc.docs.forEach((nameDoc) => {
-      //Head
-      let thead = document.createElement("thead");
-      let tr = document.createElement("tr");
-      let th1 = document.createElement("th");
-      let th2 = document.createElement("th");
-      let th3 = document.createElement("th");
-
-      th1.document.createTextNode("Name");
-      th2.document.createTextNode("File Size");
-      th3.document.createTextNode("Date");
-
-      tr.appendChild(th1);
-      tr.appendChild(th2);
-      tr.appendChild(th3);
-
-      thead.appendChild(tr);
-
       //Body
 
-      let tbody = document.createElement("tbody");
-
-      let tr_body = document.createElement("tr");
+      let tr = document.createElement("tr");
       let td1 = document.createElement("td");
       let td2 = document.createElement("td");
       let td3 = document.createElement("td");
       let td4 = document.createElement("td");
 
-      td1.createTextNode("Name");
+      //Añadir datos
+      td1.appendChild(document.createTextNode(nameDoc.name));
       td1.classList.add("name_file");
-      td2.createTextNode("File Size");
+      td2.appendChild(document.createTextNode("18 KB"));
       td2.classList.add("size_file");
-      td3.createTextNode("Date");
+      td3.appendChild(document.createTextNode("24/05/2021"));
       td3.classList.add("date_file");
 
       td4.classList.add("options_file");
       td4.classList.add("row" + count);
       let i = document.createElement("i");
-      i.classList.add("fas fa-ellipsis-v");
+      i.classList.add("fas");
+      i.classList.add("fa-ellipsis-v");
       td4.appendChild(i);
 
-      //Añadir datos
-      td1.appendChild(document.createTextNode(nameDoc.name));
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      tr.appendChild(td3);
+      tr.appendChild(td4);
+      tr.appendChild(create_options(count));
 
-      tr_body.appendChild(td1);
-      tr_body.appendChild(td2);
-      tr_body.appendChild(td3);
-      tr_body.appendChild(td4);
-      tr_body.appendChild(create_options(count));
+      count++;
 
-      tbody.appendChild(tr_body);
+      tbody.appendChild(tr);
     });
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
 
     div_list.appendChild(table);
 
@@ -88,15 +93,18 @@ function readDocuments() {
     let ul = document.createElement("ul"),
       li1 = document.createElement("li"),
       i1 = document.createElement("i");
-    i1.classList.add("fas fa-download");
+    i1.classList.add("fas");
+    i1.classList.add("fa-download");
     li1.appendChild(i1);
     let li2 = document.createElement("li"),
       i2 = document.createElement("i");
-    i2.classList.add("fas fa-edit");
+    i2.classList.add("fas");
+    i2.classList.add("fa-edit");
     li2.appendChild(i2);
     let li3 = document.createElement("li"),
       i3 = document.createElement("i");
-    i3.classList.add("fas fa-trash");
+    i3.classList.add("fas");
+    i3.classList.add("fa-trash");
     li3.appendChild(i3);
 
     ul.appendChild(li1);
