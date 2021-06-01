dc0a9858e42c630c4ffd7e5b112944b04cb1a4d3
Samuel <samuel.isip26@gmail.com>
2021-06-01 13:34:37 +0200

    Download selected files

---
+++ api/download_select.php
@@ -0,0 +1,37 @@
+<?php
+    
+    include_once("backup_inc.php");
+
+    $folder = $_GET['folder'];
+
+    chdir("./..".$folder);
+
+    if($folder=="/"){
+        $folder="/Documents";
+    }
+
+
+    $arr_files = json_decode($_GET['files']);
+
+    //Nombre de la carpeta
+    $filenameZip = basename($folder).".zip";
+
+    $archivoBackup=new ZipArchive;
+    
+    $archivoBackup->open($filenameZip,ZipArchive::CREATE);
+
+    for ($i=0; $i < count($arr_files); $i++) { 
+        $archivoBackup->addFile($arr_files[$i]);
+    }
+
+    $archivoBackup->close();
+
+    header("content-type:application/zip");
+    header("content-disposition:attachment;filename=".$filenameZip); 
+    readfile($filenameZip); 
+
+    //Eliminamos el archivo de nuestro servidor
+    unlink($filenameZip);
+    
+
+?>
