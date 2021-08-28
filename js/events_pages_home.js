18406274836af9300e19e3f5721b9d314d022c81
Samuel <samuel.isip26@gmail.com>
2021-08-28 17:44:13 +0200

    Solve some bugs

--- js/events_pages_home.js
+++ js/events_pages_home.js
@@ -1,37 +1,67 @@
 function load_events_pages_menu() {
-  document.getElementById("documents").addEventListener("click", () => {
+  $("#documents").on("click", () => {
+    $("#container_docs").css("display", "flex");
+    $(
+      "#container_images, #container_calendar, #container_bookmarks, #container_tasks"
+    ).css("display", "none");
+  });
+  $("#images").on("click", () => {
+    $("#container_images").css("display", "flex");
+    $(
+      "#container_docs, #container_calendar, #container_bookmarks, #container_tasks"
+    ).css("display", "none");
+  });
+  $("#calendar").on("click", () => {
+    $("#container_calendar").css("display", "flex");
+    $(
+      "#container_docs, #container_images, #container_bookmarks, #container_tasks"
+    ).css("display", "none");
+  });
+  $("#bookmark").on("click", () => {
+    $("#container_bookmarks").css("display", "flex");
+    $(
+      "#container_docs, #container_images, #container_calendar, #container_tasks"
+    ).css("display", "none");
+  });
+  $("#task").on("click", () => {
+    $("#container_tasks").css("display", "flex");
+    $(
+      "#container_docs, #container_images, #container_calendar, #container_bookmarks"
+    ).css("display", "none");
+  });
+  /* document.getElementById("documents").addEventListener("click", () => {
     document.getElementById("container_docs").style.display = "flex";
     document.getElementById("container_images").style.display = "none";
     document.getElementById("container_calendar").style.display = "none";
     document.getElementById("container_bookmarks").style.display = "none";
     document.getElementById("container_tasks").style.display = "none";
-  });
-  document.getElementById("images").addEventListener("click", () => {
+  }); */
+  /* document.getElementById("images").addEventListener("click", () => {
     document.getElementById("container_docs").style.display = "none";
     document.getElementById("container_images").style.display = "flex";
     document.getElementById("container_calendar").style.display = "none";
     document.getElementById("container_bookmarks").style.display = "none";
     document.getElementById("container_tasks").style.display = "none";
-  });
-  document.getElementById("calendar").addEventListener("click", () => {
+  }); */
+  /*  document.getElementById("calendar").addEventListener("click", () => {
     document.getElementById("container_docs").style.display = "none";
     document.getElementById("container_images").style.display = "none";
     document.getElementById("container_calendar").style.display = "flex";
     document.getElementById("container_bookmarks").style.display = "none";
     document.getElementById("container_tasks").style.display = "none";
-  });
-  document.getElementById("bookmark").addEventListener("click", () => {
+  }); */
+  /*  document.getElementById("bookmark").addEventListener("click", () => {
     document.getElementById("container_docs").style.display = "none";
     document.getElementById("container_images").style.display = "none";
     document.getElementById("container_calendar").style.display = "none";
     document.getElementById("container_bookmarks").style.display = "flex";
     document.getElementById("container_tasks").style.display = "none";
-  });
-  document.getElementById("task").addEventListener("click", () => {
+  }); */
+  /*  document.getElementById("task").addEventListener("click", () => {
     document.getElementById("container_docs").style.display = "none";
     document.getElementById("container_images").style.display = "none";
     document.getElementById("container_calendar").style.display = "none";
     document.getElementById("container_bookmarks").style.display = "none";
     document.getElementById("container_tasks").style.display = "flex";
-  });
+  }); */
 }
