2a7d32a1e71748a5a662e01fb55df222b3d58bb6
Samuel Isip <nsisip@serbatic.es>
2021-09-02 09:35:05 +0200

    Change db connection

--- api/connectDB.php
+++ api/connectDB.php
@@ -1,5 +1,5 @@
 <?php
 
-    $con=mysqli_connect("localhost","root","1234","hddrivedb") or die("No ha podido realizarse la conexión");
+    $con=mysqli_connect("localhost","admin","admin","hddrivedb") or die("No ha podido realizarse la conexión");
 
 ?>
