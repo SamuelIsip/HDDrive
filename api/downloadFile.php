cd49cc3cd7d828890a8787da3b4c83cc117f3009
Samuel Isip <nsisip@serbatic.es>
2021-09-06 19:16:27 +0200

    Prueba include

--- api/downloadFile.php
+++ api/downloadFile.php
@@ -1,8 +1,6 @@
 <?php
 
-    include_once("backup_inc.php")
-
-    chdir("./../../HDDriveHome");
+chdir("./../../HDDriveHome");
 
     //Guardamos la ruta del fichero
     $filename = "./".$_GET['nameFile'];
@@ -11,7 +9,6 @@
     //Y se comprimen en un único .zip
     if(is_dir($filename)){
         //incluimos las funciones necesarias para hacer el backup
-        include_once("backup_inc.php");
 
         chdir($filename);
 
@@ -44,6 +41,48 @@
             readfile($filename);
             exit;
         }
+    }
+
+
+    function aniadir($fich,$filenameZip){
+        $archivoBackup=new ZipArchive;
+        $archivoBackup->open($filenameZip,ZipArchive::CREATE);
+        $archivoBackup->addFile($fich);
+        $archivoBackup->close();
+    }
+    
+    function directorios($directorio,$archivos,$filenameZip){ //directorio actual y array con rutas
+    
+        $sub_dir = scandir($directorio); //guardamos todos los elementos del directorio en un array
+    
+        //eliminamos a . y ..
+        array_shift($sub_dir);
+        array_shift($sub_dir);
+    
+        //recorremos cada elemento
+        for ($i=0; $i < count($sub_dir); $i++)
+            if($directorio!=".")
+                $sub_dir[$i] = $directorio.$sub_dir[$i]; 
+    
+        $archivos = array_merge($archivos, $sub_dir);
+    
+        $aux = true;
+    
+        while ($aux) {
+            
+            //Vamos eliminando los elmentos del array y comprobamos si es directorio o archivo
+            $directorio_archivo = array_shift($archivos);
+    
+            if($directorio_archivo != NULL)
+                if(is_dir($directorio_archivo)) //si es un directorio, se vuelve a hacer el mismo proceso recursivamente
+                    directorios($directorio_archivo."/", $archivos,$filenameZip);
+                else
+                    aniadir($directorio_archivo,$filenameZip); //añadimos los archivos al .zip
+            else
+                $aux=false;
+    
+        }
+    
     }
 
     
