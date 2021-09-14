222f0c661badaa0aa577e82e8c58d92d2e2e88d2
Samuel Isip <nsisip@serbatic.es>
2021-09-14 16:44:04 +0200

    Fix bugs

--- api/downloadFile.php
+++ api/downloadFile.php
@@ -1,5 +1,6 @@
 <?php
 
+    include_once("backup_inc.php");
 
     chdir("./../../HDDriveHome");
 
@@ -44,46 +45,7 @@
         }
     }
     
-    function aniadir($fich,$filenameZip){
-        $archivoBackup=new ZipArchive;
-        $archivoBackup->open($filenameZip,ZipArchive::CREATE);
-        $archivoBackup->addFile($fich);
-        $archivoBackup->close();
-    }
-    
-    function directorios($directorio,$archivos,$filenameZip){ //directorio actual y array con rutas
-    
-        $sub_dir = scandir($directorio); //guardamos todos los elementos del directorio en un array
-    
-        //eliminamos a . y ..
-        array_shift($sub_dir);
-        array_shift($sub_dir);
-    
-        //recorremos cada elemento
-        for ($i=0; $i < count($sub_dir); $i++)
-            if($directorio!=".")
-                $sub_dir[$i] = $directorio.$sub_dir[$i]; 
-    
-        $archivos = array_merge($archivos, $sub_dir);
-    
-        $aux = true;
-    
-        while ($aux) {
-            
-            //Vamos eliminando los elmentos del array y comprobamos si es directorio o archivo
-            $directorio_archivo = array_shift($archivos);
-    
-            if($directorio_archivo != NULL)
-                if(is_dir($directorio_archivo)) //si es un directorio, se vuelve a hacer el mismo proceso recursivamente
-                    directorios($directorio_archivo."/", $archivos,$filenameZip);
-                else
-                    aniadir($directorio_archivo,$filenameZip); //añadimos los archivos al .zip
-            else
-                $aux=false;
-    
-        }
-    
-    }
+
 
 
 ?>
