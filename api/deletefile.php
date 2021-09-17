e919153aae7a8217cfe10408a667746f5243368a
Samuel Isip <nsisip@serbatic.es>
2021-09-17 11:55:57 +0200

    Some changes in files

--- api/deletefile.php
+++ api/deletefile.php
@@ -18,18 +18,18 @@
     } 
 
     function rrmdir($dir) { 
-      
-        $directorios = scandir($dir);
-        foreach ($directorios = as $dir) { 
-          if ($dir != "." && $dir != "..") { 
-            if (is_dir($dir. DIRECTORY_SEPARATOR .$dir) && !is_link($dir."/".$dir))
-              rrmdir($dir. DIRECTORY_SEPARATOR .$dir);
-            else
-              unlink($dir. DIRECTORY_SEPARATOR .$dir); 
-          } 
-        }
-        rmdir($dir); 
-      
+        if (is_dir($dir)) { 
+          $objects = scandir($dir);
+          foreach ($objects as $object) { 
+            if ($object != "." && $object != "..") { 
+              if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object))
+                rrmdir($dir. DIRECTORY_SEPARATOR .$object);
+              else
+                unlink($dir. DIRECTORY_SEPARATOR .$object); 
+            } 
+          }
+          rmdir($dir); 
+        } 
     }
 
 ?>
