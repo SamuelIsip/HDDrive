a29f7905d66da60c3e31fdb51d2ca9102c7e2769
Samuel Isip <nsisip@serbatic.es>
2021-09-17 12:15:01 +0200

    Dropzone imports

---
+++ js/dropzone_docs.js
@@ -0,0 +1,11 @@
+import Dropzone from "dropzone";
+
+// Make sure Dropzone doesn't try to attach itself to the
+// element automatically.
+// This behaviour will change in future versions.
+Dropzone.autoDiscover = false;
+
+let myDropzone = new Dropzone("#my-form");
+myDropzone.on("addedfile", (file) => {
+  console.log(`File added: ${file.name}`);
+});
