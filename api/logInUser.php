675e8e14f2353a735fa0a9d24f6d2f22b2f23e7d
Samuel <samuel.isip26@gmail.com>
2021-09-21 17:53:34 +0200

    Implement Log In

---
+++ api/logInUser.php
@@ -0,0 +1,22 @@
+<?php
+
+    include_once("connectDB.php");
+
+    $user_data = file_get_contents('php://input');
+
+    $user_data = json_decode($user_data,true);
+
+    //Consultar si ese usuario existe
+    $result = mysqli_query($con, "SELECT email, password FROM User;");
+
+    for ($i=1; mysqli_num_rows($result) >= $i; $i++){
+        $fila = mysqli_fetch_row($result);
+        if($fila[0] == $user_data["email"] && $fila[1] == $user_data["password"]){
+            mysqli_close($con);
+            http_response_code(200);
+            exit;
+        }
+    }
+
+    
+?>
