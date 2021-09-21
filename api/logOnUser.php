9f37dcb8af013515e013ab3304e86079900392f7
Samuel <samuel.isip26@gmail.com>
2021-09-21 16:38:03 +0200

    Fix Log On Bug

--- api/logOnUser.php
+++ api/logOnUser.php
@@ -6,13 +6,33 @@
 
     $user_data = json_decode($user_data,true);
 
-    $arr_datos = array();
+    //Consultar si ese usuario existe
 
-    array_push($arr_datos,$_POST['json_user']);
-    array_push($arr_datos,$user_data);
+    $result = mysqli_query($con, "SELECT email FROM User;");
 
-    http_response_code(200); 
-   
-    echo json_encode($user_data["email"]);
+    for ($i=1; mysqli_num_rows($result) >= $i; $i++){
+        $fila = mysqli_fetch_row($result);
+        if($fila[0] == $user_data["email"]){
+            mysqli_close($con);
+            http_response_code(409);
+            exit;
+        }
+    }
 
+    $stmt = mysqli_prepare($con, "INSERT INTO User (name,email,password,nom_usr,tlf,date) VALUES(?,?,?,?,?,?)");
+
+    $modified=date("d/m/Y");
+
+    mysqli_stmt_bind_param($stmt,"ssssss",$user_data["name"],$user_data["email"],$user_data["password"],$user_data["name_user"],$user_data["phone"],$modified);
+
+    //Ejecutamos la consulta
+    if(mysqli_stmt_execute($stmt))
+        http_response_code(200); 
+    else
+        http_response_code(500); 
+
+    //Liberamos recurso
+    mysqli_stmt_close($stmt);
+
+    mysqli_close($con);
 ?>
