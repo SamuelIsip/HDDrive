b45f38e57acc612f9a0f28ae88a1e5450a279649
Samuel <samuel.isip26@gmail.com>
2021-09-21 18:23:10 +0200

    Implement Log In

--- api/logInUser.php
+++ api/logInUser.php
@@ -7,13 +7,16 @@
     $user_data = json_decode($user_data,true);
 
     //Consultar si ese usuario existe
-    $result = mysqli_query($con, "SELECT email, password FROM User;");
+    $result = mysqli_query($con, "SELECT email, password, nom_usr FROM User;");
 
     for ($i=1; mysqli_num_rows($result) >= $i; $i++){
         $fila = mysqli_fetch_row($result);
         if($fila[0] == $user_data["email"] && $fila[1] == $user_data["password"]){
             mysqli_close($con);
             http_response_code(200);
+            if(!is_dir("./../../HDDriveHome/".$fila[2])){
+                mkdir("./../../HDDriveHome/".$fila[2],0777);
+            }
             exit;
         }
     }
