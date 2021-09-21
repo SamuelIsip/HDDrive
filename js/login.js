d6bc09958576ed6b00b1e8e1ee97b4112fe714a6
Samuel <samuel.isip26@gmail.com>
2021-09-21 17:48:12 +0200

    Implement Log In

--- js/login.js
+++ js/login.js
@@ -1,1 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("login_button").addEventListener("click", () => {
+    //Data of user
+    const user_data = {
+      email: document.getElementById("login_email").value,
+      password: document.getElementById("login_pass").value,
+    };
+
+    //Send data to server
+
+    if (window.ActiveXObject) xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    else xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        window.location = encodeURI("./../HDDrive/pages/home.php");
+      }
+    };
+
+    xhr.open("POST", "./../HDDrive/api/logInUser.php", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(user_data));
+  });
+});
