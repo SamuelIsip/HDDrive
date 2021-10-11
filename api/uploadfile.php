74c5556ddbd6782e467076d74184b651276abd8e
Samuel Isip <nsisip@serbatic.es>
2021-10-12 01:33:25 +0200

    Upload multiple file fix

--- api/uploadfile.php
+++ api/uploadfile.php
@@ -2,35 +2,41 @@
     session_name("userSession");
     session_start();
 
-    move_uploaded_file($_FILES['add_file']['name'], "./../../HDDriveHome/".$_SESSION['userName']."/".$_POST['rutaDir'].$_FILES['add_file']['name']);
+    $arrFiles = $_FILES['file'];
 
-    chdir("./../../HDDriveHome/".$_SESSION['userName']."/".$_POST['rutaDir']);
+    foreach ($arrFiles as $file){
 
-    $value = $_FILES['file']['name'];
+        move_uploaded_file($file['tmp_name'], "./../../HDDriveHome/".$_SESSION['userName']."/".$_POST['rutaDir'].$file['name']);
 
-    include_once("connectDB.php");
-    include_once("getSizeFile.php");
+        chdir("./../../HDDriveHome/".$_SESSION['userName']."/".$_POST['rutaDir']);
 
-    if(is_file($value)){
-        //Insertamos datos de fichero a la BD
-        $stmt=mysqli_prepare($con, "INSERT INTO folders (id_user,name,date,modified,size) VALUES(?,?,?,?,?)");
+        $value = $file['name'];
 
-        //Definimos parametros de la consulta
-        //La funcion solo admite variables
-        $id_user = $_SESSION["userID"];
-        $name=$value;
-        $date=date("d/m/Y");
-        $modified=date ("d/m/Y", filemtime($value));
-        $size = convert_size(filesize($value));
-        mysqli_stmt_bind_param($stmt,"issss",$id_user,$name,$date,$modified,$size);
+        include_once("connectDB.php");
+        include_once("getSizeFile.php");
 
-        //Ejecutamos la consulta
-        mysqli_stmt_execute($stmt);
+        if(is_file($value)){
+            //Insertamos datos de fichero a la BD
+            $stmt=mysqli_prepare($con, "INSERT INTO folders (id_user,name,date,modified,size) VALUES(?,?,?,?,?)");
 
-        //Liberamos recurso
-        mysqli_stmt_close($stmt);
+            //Definimos parametros de la consulta
+            //La funcion solo admite variables
+            $id_user = $_SESSION["userID"];
+            $name=$value;
+            $date=date("d/m/Y");
+            $modified=date ("d/m/Y", filemtime($value));
+            $size = convert_size(filesize($value));
+            mysqli_stmt_bind_param($stmt,"issss",$id_user,$name,$date,$modified,$size);
+
+            //Ejecutamos la consulta
+            mysqli_stmt_execute($stmt);
+
+            //Liberamos recurso
+            mysqli_stmt_close($stmt);
+        }
+
+        mysqli_close($con);
+
     }
-
-    mysqli_close($con);
 
 ?>
