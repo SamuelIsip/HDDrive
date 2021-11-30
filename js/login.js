97a42d76208f3df09dfa195b1208861f0527e33d
PC-Serbatic <samuel.isip62@gmail.com>
2021-12-01 00:22:25 +0100

    Enter in login

--- js/login.js
+++ js/login.js
@@ -1,53 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("login_pass").addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("login_button").click();
+    }
+  });
   document
     .getElementById("login_button")
-    .addEventListener("click", function (event) {
-      //Data of user
-      const user_data = {
-        email: document.getElementById("login_email").value,
-        password: document.getElementById("login_pass").value,
-      };
-
-      if (
-        document.getElementById("login_error_info").className ==
-        "login_error_info_on"
-      ) {
-        document.getElementById("login_error_info").className =
-          "login_error_info_off";
-        removeErrorColor();
-      }
-
-      //Validate de data of LogIn
-      if (validateLogIn(user_data) == false) {
-        return false;
-      }
-
-      //Send data to server
-      if (window.ActiveXObject) xhr = new ActiveXObject("Microsoft.XMLHTTP");
-      else xhr = new XMLHttpRequest();
-
-      xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          const dataUsr = JSON.parse(this.responseText);
-          sessionStorage.setItem("userName", dataUsr.nom_usr);
-          sessionStorage.setItem("userID", dataUsr.id_user);
-          window.location = encodeURI("./../HDDrive/pages/home.php");
-        } else if (this.readyState == 4 && this.status == 404) {
-          document.getElementById("login_email").style.border =
-            "1px solid #ff0000";
-          document.getElementById("login_pass").style.border =
-            "1px solid #ff0000";
-          document.getElementById("login_error_info").className =
-            "login_error_info_on";
-          document.getElementById("login_error_info").innerHTML =
-            "Email/Password are incorrect!";
-        }
-      };
-
-      xhr.open("POST", "./../HDDrive/api/logInUser.php", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      xhr.send(JSON.stringify(user_data));
+    .addEventListener("click", function () {
+      loginHome();
     });
+
+  function loginHome() {
+    //Data of user
+    const user_data = {
+      email: document.getElementById("login_email").value,
+      password: document.getElementById("login_pass").value,
+    };
+
+    if (
+      document.getElementById("login_error_info").className ==
+      "login_error_info_on"
+    ) {
+      document.getElementById("login_error_info").className =
+        "login_error_info_off";
+      removeErrorColor();
+    }
+
+    //Validate de data of LogIn
+    if (validateLogIn(user_data) == false) {
+      return false;
+    }
+
+    //Send data to server
+    if (window.ActiveXObject) xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    else xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        const dataUsr = JSON.parse(this.responseText);
+        sessionStorage.setItem("userName", dataUsr.nom_usr);
+        sessionStorage.setItem("userID", dataUsr.id_user);
+        window.location = encodeURI("./../HDDrive/pages/home.php");
+      } else if (this.readyState == 4 && this.status == 404) {
+        document.getElementById("login_email").style.border =
+          "1px solid #ff0000";
+        document.getElementById("login_pass").style.border =
+          "1px solid #ff0000";
+        document.getElementById("login_error_info").className =
+          "login_error_info_on";
+        document.getElementById("login_error_info").innerHTML =
+          "Email/Password are incorrect!";
+      }
+    };
+
+    xhr.open("POST", "./../HDDrive/api/logInUser.php", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(user_data));
+  }
 
   /* Validación del Email y Password */
   function validateLogIn(user_data) {
