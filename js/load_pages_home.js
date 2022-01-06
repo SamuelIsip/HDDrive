f03341e0e96a679e133b027c75a5be3116989c34
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-06 15:25:13 +0100

    Go back function

--- js/load_pages_home.js
+++ js/load_pages_home.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener(
+    "popstate",
+    function (event) {
+      var r = confirm("You pressed a Back button! Are you sure?!");
+
+      if (r == true) {
+        $("#container_docs").css("display", "flex");
+        $("#container_calendar, #container_favourites, #container_tasks").css(
+          "display",
+          "none"
+        );
+
+        // Remove the table, to reload files of index
+        deleteFileRecursive();
+        readDocuments();
+        resetLinkHead();
+      }
+    },
+    false
+  );
   full_size_monitor();
   readDocuments();
   load_events_pages_menu();
