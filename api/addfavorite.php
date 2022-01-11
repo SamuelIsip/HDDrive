db249fa8de0678a424b3975c1c435a996f31eb55
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-12 00:16:00 +0100

    Add selected files to favourites

--- api/addfavorite.php
+++ api/addfavorite.php
@@ -5,52 +5,61 @@
     session_name("userSession");
     session_start();
 
-    $user_data = file_get_contents('php://input');
+    $file_data = file_get_contents('php://input');
 
-    $user_data = json_decode($user_data,true);
+    $file_data = json_decode($file_data);
 
-    //Definimos parametros de la consulta
-    //La funcion solo admite variables
-    $id_user = $_SESSION["userID"];
-    $size = $user_data["size"];
-    $date = $user_data["date"];
-    $ruta = $user_data["ruta"];
-    $name_file = $user_data["name"];
-    
-    // Conseguir el id_folder
-    $stmt = mysqli_prepare($con, "SELECT id_folder FROM folders WHERE id_user = ? AND name = ?;");
+    $user_data = array();
 
-    mysqli_stmt_bind_param($stmt,"ss", $id_user, $name_file);
+    foreach ($file_data as $file) { 
+        $user_data = json_decode($file,true);
+        addToFavourite($user_data);
+    }
 
-    //Ejecutamos la consulta
-    if(mysqli_stmt_execute($stmt)){
+    function addToFavourite($user_data){
+        //Definimos parametros de la consulta
+        //La funcion solo admite variables
+        $id_user = $_SESSION["userID"];
+        $size = $user_data["size"];
+        $date = $user_data["date"];
+        $ruta = $user_data["ruta"];
+        $name_file = $user_data["name"];
+        
+        // Conseguir el id_folder
+        $stmt = mysqli_prepare($con, "SELECT id_folder FROM folders WHERE id_user = ? AND name = ?;");
 
-        mysqli_stmt_bind_result($stmt, $id_folder);
+        mysqli_stmt_bind_param($stmt,"ss", $id_user, $name_file);
 
-        if(mysqli_stmt_fetch($stmt)){
-            //Liberamos recurso
-            mysqli_stmt_close($stmt);
-            //Insertamos datos de fichero a la BD
-            $stmt=mysqli_prepare($con, "INSERT INTO favorites (id_user, date, size, ruta, id_folder) VALUES(?,?,?,?,?)");
-            
-            mysqli_stmt_bind_param($stmt,"isssi",$id_user,$date,$size,$ruta,$id_folder);
+        //Ejecutamos la consulta
+        if(mysqli_stmt_execute($stmt)){
 
-            //Ejecutamos la consulta
-            if(mysqli_stmt_execute($stmt)){
-                 //Liberamos recurso
+            mysqli_stmt_bind_result($stmt, $id_folder);
+
+            if(mysqli_stmt_fetch($stmt)){
+                //Liberamos recurso
                 mysqli_stmt_close($stmt);
-                mysqli_close($con);
-                http_response_code(200); 
-                exit;
-            }else
-                http_response_code(500); 
-        }
-    }else
-        http_response_code(501); 
-    
-    //Liberamos recurso
-    mysqli_stmt_close($stmt);
+                //Insertamos datos de fichero a la BD
+                $stmt=mysqli_prepare($con, "INSERT INTO favorites (id_user, date, size, ruta, id_folder) VALUES(?,?,?,?,?)");
+                
+                mysqli_stmt_bind_param($stmt,"isssi",$id_user,$date,$size,$ruta,$id_folder);
 
-    mysqli_close($con);
+                //Ejecutamos la consulta
+                if(mysqli_stmt_execute($stmt)){
+                    //Liberamos recurso
+                    mysqli_stmt_close($stmt);
+                    mysqli_close($con);
+                    http_response_code(200); 
+                    exit;
+                }else
+                    http_response_code(500); 
+            }
+        }else
+            http_response_code(501); 
+        
+        //Liberamos recurso
+        mysqli_stmt_close($stmt);
+
+        mysqli_close($con);
+    }
 
 ?>
--- js/add_select_files.js
+++ js/add_select_files.js
@@ -116,7 +116,7 @@ function load_events_add_select() {
           ruta: getPath() + nameFolder,
         };
 
-        arrFiles.push(fileData);
+        arrFiles.push(JSON.stringify(fileData));
       });
 
       var json_arr = JSON.stringify(arrFiles);
