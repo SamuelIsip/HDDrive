f58e76eae73ad91e3d864cdce2f08cf868f3b27f
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-13 13:56:39 +0100

    Implementation of password restore

---
+++ api/sendRestoreCode.php
@@ -0,0 +1,53 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/phpmailer/src/Exception.php';
+    require 'phpmailer/phpmailer/src/PHPMailer.php';
+    require 'phpmailer/phpmailer/src/SMTP.php';
+
+    $email = $_POST['emailUser'];
+
+    enviarEmail($email);
+
+    function enviarEmail($email){
+
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = 2;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'samuel.isip62@gmail.com';                     //SMTP username
+            $mail->Password   = 'SecureMan2*112_S';                               //SMTP password
+            $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+            $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTP>
+            //Recipients
+            $mail->setFrom('samuel.isip62@gmail.com', 'Mailer');
+            $mail->addAddress($email, 'Mailer');     //Add a recipient
+            //$mail->addAddress('ellen@example.com');               //Name is optional
+            //$mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Prueba de correo desde php';
+            $mail->Body    = 'Soy Samuel. This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';                                                                                       
+        } catch (Exception $e) {                                                                                                    
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+?>
---
+++ js/restorePassword.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let email = document.getElementById("restoreEmail").value;
+
+  sendCode(email);
+
+  async function sendCode(email) {
+    const response = await fetch("./../HDDrive/api/sendRestoreCode.php", {
+      method: "POST",
+      cache: "no-cache",
+      headers: { "Content-Type": "application/json" },
+      body: { emailUser: email },
+    });
+    if (response.ok) {
+      let resp = await response.text();
+      console.log(resp);
+      document.getElementById("restoreCode").disabled = false;
+      document.getElementById("restoreNewPassword").disabled = false;
+      document.getElementById(
+        "restoreNewPasswordConfirmation"
+      ).disabled = false;
+      document.getElementById("btnChangePass").disabled = false;
+    } else {
+      alert("The code could not be sent");
+    }
+  }
+});
