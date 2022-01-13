32707362e15ce83cba52f596baa5693058736c32
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-13 18:04:04 +0100

    Send code restore password

--- js/restorePassword.js
+++ js/restorePassword.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
+  var restorePassCode = 0;
+  var email = "";
+
   document.getElementById("btnSendCode").addEventListener("click", () => {
-    var email = document.getElementById("restoreEmail").value;
+    email = document.getElementById("restoreEmail").value;
     const emailJson = { emailJSON: email };
-    var code = sendCode(emailJson);
-    console.log(code);
+    restorePassCode = sendCode(emailJson);
+  });
+
+  document.getElementById("btnChangePass").addEventListener("click", () => {
+    var newPass = document.getElementById("restoreNewPassword").value;
+    var newConfirmationPass = document.getElementById(
+      "restoreNewPasswordConfirmation"
+    ).value;
+    sendDataToCheckAndRestorePass(restorePassCode, newPass, email);
   });
 
   async function sendCode(emailJson) {
@@ -14,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
       body: JSON.stringify(emailJson),
     });
     if (response.ok) {
-      var resp = await response.text();
+      var code = await response.text();
       console.log(resp);
       document.getElementById("restoreCode").disabled = false;
       document.getElementById("restoreNewPassword").disabled = false;
@@ -26,6 +36,28 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       alert("The code could not be sent");
       return 0;
+    }
+  }
+
+  async function sendDataToCheckAndRestorePass(
+    restorePassCode,
+    newPass,
+    email
+  ) {
+    const dataUser = {
+      restorePassCode: restorePassCode,
+      newPass: newPass,
+      email: email,
+    };
+    const response = await fetch("./../api/changePassword.php", {
+      method: "POST",
+      cache: "no-cache",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataUser),
+    });
+    if (response.ok) {
+    } else {
+      alert("The password could not be changed");
     }
   }
 });
