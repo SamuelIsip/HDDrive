20f7b17f7cc8ae7de4bb308fab202bd660bf67fa
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-17 13:58:40 +0100

    Change Password php

--- api/changePassword.php
+++ api/changePassword.php
@@ -10,9 +10,6 @@
     $email = $_GET['email'];
     $newPass = password_hash($_GET['newPass'],PASSWORD_DEFAULT);
 
-    echo $email;
-    echo $newPass;
-
     $stmt = mysqli_prepare($con, "SELECT email FROM User WHERE email = ?");
 
     mysqli_stmt_bind_param($stmt, "s", $email);
@@ -29,6 +26,13 @@
 
     //Liberamos recurso
     mysqli_stmt_close($stmt);
+
+
+    $stmt = mysqli_prepare($con, "UPDATE User SET password = ? WHERE email = ?");
+    mysqli_stmt_bind_param($stmt, "ss", $newPass, $email);
+    mysqli_stmt_execute($stmt);
+    echo(mysqli_stmt_affected_rows($stmt));
+
     mysqli_close($con);
     http_response_code(500);
 
