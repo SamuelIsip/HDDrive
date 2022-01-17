992b6fee524d6453389511b869d1183d3a6a35ec
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-17 17:24:18 +0100

    Home Images notification

--- js/task_load_db.js
+++ js/task_load_db.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("task").addEventListener("click", () => {
     document.getElementById("tasks").contentWindow.add_tasks();
   });
+  document.getElementById("tasks_menu").addEventListener("click", () => {
+    document.getElementById("tasks").contentWindow.add_tasks();
+  });
 });
