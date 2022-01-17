3b5d24c7b3922e4247db2fd76d772242ba5a04de
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-17 15:38:17 +0100

    Change Password php

--- api/changePassword.php
+++ api/changePassword.php
@@ -2,38 +2,43 @@
 
     include_once("connectDB.php");
 
-    //$user_data = file_get_contents('php://input');
+    $user_data = file_get_contents('php://input');
 
-    //$user_data = json_decode($user_data,true);
+    $user_data = json_decode($user_data,true);
 
+    $email = $user_data['email'];
+    $newPass = password_hash($user_data['newPass'],PASSWORD_DEFAULT);
+    
     //Consultar si ese usuario existe
-    $email = $_GET['email'];
-    $newPass = password_hash($_GET['newPass'],PASSWORD_DEFAULT);
-
     $stmt = mysqli_prepare($con, "SELECT email FROM User WHERE email = ?");
 
     mysqli_stmt_bind_param($stmt, "s", $email);
 
-    mysqli_stmt_execute($stmt);
-    echo(mysqli_stmt_affected_rows($stmt));
+    mysqli_stmt_store_result($stmt);
 
-    mysqli_stmt_bind_result($stmt, $emailP);
+    if(mysqli_stmt_execute($stmt) && (mysqli_stmt_affected_rows($stmt) == 1)){
+        //Liberamos recurso
+        mysqli_stmt_close($stmt);
+        
+        //Actualizar contraseña
+        $stmt = mysqli_prepare($con, "UPDATE User SET password = ? WHERE email = ?");
+        mysqli_stmt_bind_param($stmt, "ss", $newPass, $email);
 
-    mysqli_stmt_fetch($stmt);
+        if(mysqli_stmt_execute($stmt)){
+            http_response_code(200);
+            cerrarConexiones($con, $stmt);
+        }else{
+            http_response_code(500); //Error al actualizar
+            cerrarConexiones($con, $stmt);
+        }
+    }else{
+        http_response_code(404); //Email no encontrado
+        cerrarConexiones($con, $stmt);
+    }
 
-    echo($emailP);
-    
-
-    //Liberamos recurso
-    mysqli_stmt_close($stmt);
-
-
-    $stmt = mysqli_prepare($con, "UPDATE User SET password = ? WHERE email = ?");
-    mysqli_stmt_bind_param($stmt, "ss", $newPass, $email);
-    mysqli_stmt_execute($stmt);
-    echo(mysqli_stmt_affected_rows($stmt));
-
-    mysqli_close($con);
-    http_response_code(500);
-
+    function cerrarConexiones($con, $stmt){
+        mysqli_stmt_close($stmt);
+        mysqli_close($con);
+        exit;
+    }
 ?>
