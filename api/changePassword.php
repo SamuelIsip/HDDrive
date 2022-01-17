105a89f79d250aada471ac1d44354dfebf090f4b
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-17 13:56:38 +0100

    Change Password php

--- api/changePassword.php
+++ api/changePassword.php
@@ -7,26 +7,25 @@
     //$user_data = json_decode($user_data,true);
 
     //Consultar si ese usuario existe
-    $email = $_GET[['email'];
+    $email = $_GET['email'];
     $newPass = password_hash($_GET['newPass'],PASSWORD_DEFAULT);
+
+    echo $email;
+    echo $newPass;
 
     $stmt = mysqli_prepare($con, "SELECT email FROM User WHERE email = ?");
 
     mysqli_stmt_bind_param($stmt, "s", $email);
 
-    if(mysqli_stmt_execute($stmt) && (mysqli_stmt_affected_rows($stmt) == 1)){
-        $stmt = mysqli_prepare($con, "UPDATE User SET password = ? WHERE email = ?");
-        mysqli_stmt_bind_param($stmt, "ss", $newPass, $email);
-        if(mysqli_stmt_execute($stmt)){
-            //Liberamos recurso
-            mysqli_stmt_close($stmt);
-            mysqli_close($con);
-            http_response_code(200);
-            echo "Updated password successfully!";
-        }
-    }else{
-        echo "User does not exist!";
-    }
+    mysqli_stmt_execute($stmt);
+    echo(mysqli_stmt_affected_rows($stmt));
+
+    mysqli_stmt_bind_result($stmt, $emailP);
+
+    mysqli_stmt_fetch($stmt);
+
+    echo($emailP);
+    
 
     //Liberamos recurso
     mysqli_stmt_close($stmt);
