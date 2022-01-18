f06ef25c1ed577249f30aa819228408d44144295
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-18 16:08:16 +0100

    Images implementation

--- js/images.js
+++ js/images.js
@@ -1,15 +1,15 @@
-let modalId = document.querySelector("#image-gallery");
+let modalId = $("#image-gallery");
 
-document.addEventListener("DOMContentLoaded", () => {
+$(function () {
   loadGallery(true, "a.thumbnail");
 
   //This function disables buttons when needed
   function disableButtons(counter_max, counter_current) {
-    document.querySelector("#show-previous-image, #show-next-image").show();
+    $("#show-previous-image, #show-next-image").show();
     if (counter_max === counter_current) {
-      document.querySelector("#show-next-image").hide();
+      $("#show-next-image").hide();
     } else if (counter_current === 1) {
-      document.querySelector("#show-previous-image").hide();
+      $("#show-previous-image").hide();
     }
   }
 
@@ -24,61 +24,55 @@ document.addEventListener("DOMContentLoaded", () => {
       selector,
       counter = 0;
 
-    document
-      .querySelector("#show-next-image, #show-previous-image")
-      .click(function () {
-        if (document.querySelector(this).attr("id") === "show-previous-image") {
-          current_image--;
-        } else {
-          current_image++;
-        }
+    $("#show-next-image, #show-previous-image").click(function () {
+      if ($(this).attr("id") === "show-previous-image") {
+        current_image--;
+      } else {
+        current_image++;
+      }
 
-        selector = document.querySelector(
-          '[data-image-id="' + current_image + '"]'
-        );
-        updateGallery(selector);
-      });
+      selector = $('[data-image-id="' + current_image + '"]');
+      updateGallery(selector);
+    });
 
     function updateGallery(selector) {
       let $sel = selector;
       current_image = $sel.data("image-id");
-      document.querySelector("#image-gallery-title").text($sel.data("title"));
-      document
-        .querySelector("#image-gallery-image")
-        .attr("src", $sel.data("image"));
+      $("#image-gallery-title").text($sel.data("title"));
+      $("#image-gallery-image").attr("src", $sel.data("image"));
       disableButtons(counter, $sel.data("image-id"));
     }
 
     if (setIDs == true) {
-      document.querySelector("[data-image-id]").forEach(function () {
+      $("[data-image-id]").each(function () {
         counter++;
-        document.querySelector(this).attr("data-image-id", counter);
+        $(this).attr("data-image-id", counter);
       });
     }
-    document.querySelector(setClickAttr).addEventListener("click", function () {
-      updateGallery(document.querySelector(this));
+    $(setClickAttr).on("click", function () {
+      updateGallery($(this));
     });
   }
 });
 
 // build key actions
-document.addEventListener("keydown", function (e) {
+$(document).on("keydown", function (e) {
   switch (e.key) {
     case 37: // left
       if (
         (modalId.data("bs.modal") || {})._isShown &&
-        document.querySelector("#show-previous-image").is(":visible")
+        $("#show-previous-image").is(":visible")
       ) {
-        document.querySelector("#show-previous-image").click();
+        $("#show-previous-image").click();
       }
       break;
 
     case 39: // right
       if (
         (modalId.data("bs.modal") || {})._isShown &&
-        document.querySelector("#show-next-image").is(":visible")
+        $("#show-next-image").is(":visible")
       ) {
-        document.querySelector("#show-next-image").click();
+        $("#show-next-image").click();
       }
       break;
 
