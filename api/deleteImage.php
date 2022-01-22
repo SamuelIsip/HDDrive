1d955d175cbffcb4f830df127d21d90cfa5e72a4
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-22 02:16:48 +0100

    Drop Image implementation

--- api/deleteImage.php
+++ api/deleteImage.php
@@ -1,5 +1,7 @@
 <?php
 
+    include_once("connectDB.php");
+
     session_name("userSession");
     session_start();
 
@@ -11,7 +13,24 @@
 
     //Guardamos la ruta del fichero
     $filename = $files_data['nameFile'];
-   
+
+    //Borramos dato de la BD
+    $stmt=mysqli_prepare($con, "DELETE FROM photos WHERE id_user=? AND name=?");
+
+    //Definimos parametros de la consulta
+    //La funcion solo admite variables
+    $id_user = $_SESSION["userID"];
+    mysqli_stmt_bind_param($stmt,"is",$id_user,$filename);
+
+    //Ejecutamos la consulta
+    mysqli_stmt_execute($stmt);
+
+    //Liberamos recurso
+    mysqli_stmt_close($stmt);
+
+    mysqli_close($con);
+
+    //Borramos fichero del servidor
     unlink($filename);
       
     exit;
