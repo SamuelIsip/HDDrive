16d1a512aa29fa5978bc22d303958128ff30571b
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-24 12:21:12 +0100

    Cookies implementation on home

--- js/cookies.js
+++ js/cookies.js
@@ -13,31 +13,3 @@ function getCookie(name) {
   var match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
   return match ? match[2] : null;
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadCookie();
-  acceptCookie();
-
-  function acceptCookie() {
-    document.getElementById("button_cookie").addEventListener("click", () => {
-      const d = new Date();
-      d.setTime(d.getTime() + 2 * 24 * 60 * 60 * 1000);
-      let expires = "expires=" + d.toUTCString();
-      document.cookie = "cookieEnabled=true;" + expires + "; Path=/;";
-      document.getElementById("container_cookie").style.display = "none";
-    });
-  }
-
-  function loadCookie() {
-    if (getCookie("cookieEnabled") === null) {
-      document.getElementById(
-        "container_cookie"
-      ).style.cssText = `display: flex; 
-          flex-direction: row; 
-          justify-content: center; 
-          align-item:center;`;
-    } else {
-      document.getElementById("container_cookie").style.display = "none";
-    }
-  }
-});
--- js/login.js
+++ js/login.js
@@ -110,4 +110,31 @@ document.addEventListener("DOMContentLoaded", () => {
         logInInputsList[i].style.border = "";
     }
   }
+
+  // DISPLAY COOKIE ADVICE
+  loadCookie();
+  acceptCookie();
+
+  function acceptCookie() {
+    document.getElementById("button_cookie").addEventListener("click", () => {
+      const d = new Date();
+      d.setTime(d.getTime() + 2 * 24 * 60 * 60 * 1000);
+      let expires = "expires=" + d.toUTCString();
+      document.cookie = "cookieEnabled=true;" + expires + "; Path=/;";
+      document.getElementById("container_cookie").style.display = "none";
+    });
+  }
+
+  function loadCookie() {
+    if (getCookie("cookieEnabled") === null) {
+      document.getElementById(
+        "container_cookie"
+      ).style.cssText = `display: flex; 
+            flex-direction: row; 
+            justify-content: center; 
+            align-item:center;`;
+    } else {
+      document.getElementById("container_cookie").style.display = "none";
+    }
+  }
 });
