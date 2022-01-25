a628ec884cc4267df7d5eacdb9e7752339ea7bd5
PC-Serbatic <samuel.isip62@gmail.com>
2022-01-25 13:20:18 +0100

    Revision bug descarga ficheros

--- api/prueba.php
+++ api/prueba.php
@@ -9,51 +9,71 @@
 
     $files_selected = json_decode($_GET["files"]);
 
-    foreach($files_selected as $filename){
-        //Guardamos la ruta del fichero
-        $filename = "./".$filename;
+    //Guardamos la ruta del fichero
+    $filename = "./".$_GET["folder"];
 
-        //Si es un directorio, se recorren todo los ficheros recursivamente
-        //Y se comprimen en un único .zip
-        if(is_dir($filename)){
-            //incluimos las funciones necesarias para hacer el backup
+    //Si es un directorio, se recorren todo los ficheros recursivamente
+    //Y se comprimen en un único .zip
+    if(is_dir($filename)){
+        //incluimos las funciones necesarias para hacer el backup
 
-            chdir($filename);
+        chdir($filename);
 
-            //Nombre de la carpeta
-            $filenameZip = substr($filename,strrpos($filename, "/")+1,strlen($filename)).".zip";
+        //Nombre de la carpeta
+        $filenameZip = trim($_GET["folder"]).".zip";
 
-            $archivos=array();
+        $archivos=array();
 
-            //Llamada a la función que recorre cada directorio
-            directorios(".",$archivos,$filenameZip);
+        //Llamada a la función que recorre cada directorio
+        directorios(".",$archivos,$filenameZip, $files_selected);
 
-            header("content-type:application/zip");
-            header("content-disposition:attachment;filename=".$_GET["folder"].".zip");
-            readfile($filenameZip);
+        header("content-type:application/zip");
+        header("content-disposition:attachment;filename=".$_GET["folder"].".zip");
+        readfile($filenameZip);
 
-            //Eliminamos el archivo de nuestro servidor
-            unlink($filenameZip);
-        }
-
-        if(is_file($filename)){
-            //Comprobamos si el fichero existe
-            if(file_exists($filename)) {
-                //Definimos la información del header
-                header('Content-Description: File Transfer');
-                header('Content-Type: application/octet-stream');
-                header('Content-Disposition: attachment; filename="'.$_GET["folder"].'"');
-                header('Expires: 0');
-                header('Cache-Control: must-revalidate');
-                header('Pragma: public');
-                header('Content-Length: ' . filesize($filename));
-
-                //Leemos el fichero
-                readfile($filename);
-                exit;
-            }
-        }
+        //Eliminamos el archivo de nuestro servidor
+        unlink($filenameZip);
     }
 
+    function aniadir($fich,$filenameZip){
+        $archivoBackup=new ZipArchive;
+        $archivoBackup->open($filenameZip,ZipArchive::CREATE);
+        $archivoBackup->addFile($fich);
+        $archivoBackup->close();
+    }
+    
+    function directorios($directorio,$archivos,$filenameZip, $files_selected){ //directorio actual y array con rutas
+    
+        $sub_dir = scandir($directorio); //guardamos todos los elementos del directorio en un array
+    
+        //eliminamos a . y ..
+        array_shift($sub_dir);
+        array_shift($sub_dir);
+    
+        //recorremos cada elemento
+        for ($i=0; $i < count($sub_dir); $i++)
+            if($directorio!=".")
+                $sub_dir[$i] = $directorio.$sub_dir[$i]; 
+    
+        $archivos = array_merge($archivos, $sub_dir);
+    
+        $aux = true;
+    
+        while ($aux) {
+            
+            //Vamos eliminando los elmentos del array y comprobamos si es directorio o archivo
+            $directorio_archivo = array_shift($archivos);
+
+            if($directorio_archivo != NULL)
+                if(is_dir($directorio_archivo)) //si es un directorio, se vuelve a hacer el mismo proceso recursivamente
+                    directorios($directorio_archivo."/", $archivos,$filenameZip);
+                else
+                    aniadir($directorio_archivo,$filenameZip); //añadimos los archivos al .zip
+            else
+                $aux=false;
+
+        }
+    
+    }
 
 ?>
