6889ee2a85ea0bed0d7986ae8049593a3031d5cb
PC-Serbatic <samuel.isip62@gmail.com>
2022-02-16 23:23:47 +0100

    Loader implementation

--- js/load_pages_home.js
+++ js/load_pages_home.js
@@ -34,8 +34,17 @@ document.addEventListener("DOMContentLoaded", () => {
   function loaderDialog() {
     document.onreadystatechange = () => {
       if (document.readyState === "complete") {
-        document.getElementsByClassName("spinner")[0].style.display = "none";
+        toggleLoader();
       }
     };
+  }
+
+  function toggleLoader() {
+    var x = document.getElementsByClassName("spinner")[0];
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
 });
