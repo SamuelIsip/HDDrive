3c7c9ec42fcce4b759f2937ae07b675b1356b899
PC-Serbatic <samuel.isip62@gmail.com>
2022-02-16 23:33:07 +0100

    Loader implementation

--- js/loader.js
+++ js/loader.js
@@ -1,18 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-  function loaderDialog() {
-    document.onreadystatechange = () => {
-      if (document.readyState === "complete") {
-        toggleLoader();
-      }
-    };
-  }
-
-  function toggleLoader() {
-    var x = document.getElementsByClassName("spinner")[0];
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+function loaderDialog() {
+  document.onreadystatechange = () => {
+    if (document.readyState === "complete") {
+      toggleLoader();
     }
+  };
+}
+
+function toggleLoader() {
+  var x = document.getElementsByClassName("spinner")[0];
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
-});
+}
