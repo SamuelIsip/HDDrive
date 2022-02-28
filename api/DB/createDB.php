e79b1fc46f642e6e323fd51bb07b5deae441720e
PC-Serbatic <samuel.isip62@gmail.com>
2022-02-28 17:59:47 +0100

    Scripts for database creation

--- api/DB/createDB.php
+++ api/DB/createDB.php
@@ -1,7 +1,22 @@
 <?php
 
-    include_once("connectDB.php");
+    $con=mysqli_connect("localhost","admin","admin") or die("No ha podido realizarse la conexión");
 
-    echo($con);
+    $stmt=mysqli_prepare($con, "SELECT SCHEMA_NAME
+    FROM INFORMATION_SCHEMA.SCHEMATA
+   WHERE SCHEMA_NAME = ?");
+
+    $dbName = "hddrivedb";
+    mysqli_stmt_bind_param($stmt,"s",$dbName);
+
+    //Ejecutamos la consulta
+    mysqli_stmt_execute($stmt);
+
+    echo(mysqli_num_rows($result));
+
+    //Liberamos recurso
+    mysqli_stmt_close($stmt);
+
+    mysqli_close($con);
 
 ?>
