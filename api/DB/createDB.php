49ca4fead01e8c149583c602039755921399a885
PC-Serbatic <samuel.isip62@gmail.com>
2022-02-28 18:25:23 +0100

    Scripts for database creation

--- api/DB/createDB.php
+++ api/DB/createDB.php
@@ -17,9 +17,23 @@
     mysqli_stmt_fetch($stmt);
 
     if($nameDB === $dbName){
-        echo "Ejecutamos";
-    }else
-        echo "NO Ejecutamos";
+        echo "La Base de Datos ya existe!";
+    else{
+        
+        mysqli_query($con, "CREATE DATABASE hddrivedb2");
+        
+        mysqli_select_db($con, "hddrivedb2");
+
+        // Creamos Tablas
+        mysqli_query($con, "CREATE TABLE `favorites` (
+            `id_user` int NOT NULL,
+            `date` varchar(50) NOT NULL,
+            `size` varchar(50) DEFAULT NULL,
+            `ruta` varchar(250) NOT NULL,
+            `id_folder` int NOT NULL,
+            `id_fav` int NOT NULL
+          )");
+    }
 
     //Liberamos recurso
     mysqli_stmt_close($stmt);
