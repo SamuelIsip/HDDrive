749c544836eeda8f8d05474a7dd3a780e1a31540
PC-Serbatic <samuel.isip62@gmail.com>
2022-03-01 10:31:19 +0100

    Scripts for database creation

--- api/DB/createDB.php
+++ api/DB/createDB.php
@@ -6,17 +6,15 @@
     FROM INFORMATION_SCHEMA.SCHEMATA
     WHERE SCHEMA_NAME = ?");
 
-    $dbName ="hddrivedb2";
-    
     mysqli_stmt_bind_param($stmt,"s",$dbName);
 
     //Ejecutamos la consulta
     mysqli_stmt_execute($stmt);
 
-    mysqli_stmt_bind_result($stmt, $nameDB);
+    mysqli_stmt_bind_result($stmt, $SCHEMA_NAME);
     mysqli_stmt_fetch($stmt);
 
-    if($nameDB === $dbName){
+    if($SCHEMA_NAME === $dbName){
         echo "La Base de Datos ya existe!";
     }else{
         
