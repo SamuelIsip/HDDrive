a97f6e73cf17273e7986e384e59f88b379b25c0e
PC-Serbatic <samuel.isip62@gmail.com>
2022-03-10 16:17:04 +0100

    Delete file fix bug

--- api/deletefile.php
+++ api/deletefile.php
@@ -40,13 +40,13 @@
     function rrmdir($dir) { 
       if (is_dir($dir)) { 
         $objects = scandir($dir);
+        echo(json_encode($objects));
         foreach ($objects as $object) { 
           if ($object != "." && $object != "..") { 
             if (is_dir($dir. DIRECTORY_SEPARATOR .$object) && !is_link($dir."/".$object)){
               rrmdir($dir. DIRECTORY_SEPARATOR .$object);
             }else{
               unlink($dir. DIRECTORY_SEPARATOR .$object); 
-              echo $object;
               //dropFilesFromDB($object);
             }
           } 
