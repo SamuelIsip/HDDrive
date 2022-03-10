d4f5071edb02c607eca406da9d76c7c12abe9903
PC-Serbatic <samuel.isip62@gmail.com>
2022-03-10 17:34:45 +0100

    Create Folder for images

--- api/DB/createAdminFolders.php
+++ api/DB/createAdminFolders.php
@@ -3,6 +3,9 @@
     if(!is_dir("./../../../HDDriveHome/Admin")){
         mkdir("./../../../HDDriveHome/Admin",0777);
         mkdir("./../../../HDDriveHome/Admin/Admin",0777);
+        if(!is_dir("./../../../HDDriveHome/UserImages")){
+            mkdir("./../../../HDDriveHome/UserImages",0777);
+        }
         mkdir("./../../../HDDriveHome/UserImages/Admin",0777);
     }
 
