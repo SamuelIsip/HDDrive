539211363684b308c7ddc4d11b687e3ec48e4e50
PC-Serbatic <samuel.isip62@gmail.com>
2022-03-10 17:15:06 +0100

    Delete file fix bug

--- api/deletefile.php
+++ api/deletefile.php
@@ -21,8 +21,6 @@
       dropFilesAndDirectory($filename);
     } 
 
-    echo basename($filename);
-
     function dropFilesAndDirectory($filename){
 
       if(is_dir($filename)){
@@ -52,6 +50,48 @@
         }
         rmdir($dir); 
       } 
+    }
+
+    function dropFilesFromDB($file){
+
+        //1. Buscamos id_folder
+        $stmt=mysqli_prepare($con, "SELECT id_folder FROM folders WHERE id_user=? AND name=?");
+
+        $id_user = $_SESSION["userID"];
+        mysqli_stmt_bind_param($stmt,"is",$id_user,$file);
+
+        //Ejecutamos la consulta
+        mysqli_stmt_execute($stmt);
+
+        /* bind result variables */
+        mysqli_stmt_bind_result($stmt, $id_folder);
+
+        /* fetch values */
+        mysqli_stmt_fetch($stmt);
+
+        //2. Borramos dato de FAVORITOS
+        $stmt=mysqli_prepare($con, "DELETE FROM favorites WHERE id_user=? AND id_folder=?");
+
+        //Definimos parametros de la consulta
+        mysqli_stmt_bind_param($stmt,"ii",$id_user,$id_folder);
+
+        //Ejecutamos la consulta
+        mysqli_stmt_execute($stmt);
+
+
+        //3. Borramos dato de FOLDERS
+        $stmt=mysqli_prepare($con, "DELETE FROM folders WHERE id_user=? AND id_folder=?");
+
+        //Definimos parametros de la consulta
+        mysqli_stmt_bind_param($stmt,"ii",$id_user,$id_folder);
+
+        //Ejecutamos la consulta
+        mysqli_stmt_execute($stmt);
+
+        //Liberamos recurso
+        mysqli_stmt_close($stmt);
+
+        mysqli_close($con);
     }
     
     exit;
