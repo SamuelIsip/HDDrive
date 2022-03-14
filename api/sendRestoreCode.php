206fca77eb3294db7a05fe460f222c46d2944cfd
PC-Serbatic <samuel.isip62@gmail.com>
2022-03-14 02:11:22 +0100

    SMTP via Gmail

--- api/sendRestoreCode.php
+++ api/sendRestoreCode.php
@@ -2,6 +2,13 @@
     require_once("connectDB.php");
     require_once("mailCredentials.php");
 
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/phpmailer/src/Exception.php';
+    require 'phpmailer/phpmailer/src/PHPMailer.php';
+    require 'phpmailer/phpmailer/src/SMTP.php';
+
     $email = json_decode(file_get_contents('php://input'), true);
 
     //Consultar si ese usuario existe
@@ -25,20 +32,38 @@
     echo $code;
 
     // Función para enviar el mail con el código de confirmación
-    function enviarEmail($email, $verificationCode, $localSender){
+    function enviarEmail($email, $verificationCode, $localHost, $localUsername, $localPassword, $localSMTPSecure, $localPort){
 
-        $titulo    = 'Password change code';
-        $mensaje   = '<p>You are about to change your password.</p>
-        <p>Your <b>verification code</b> is: <b>'.$verificationCode.'</b></p>
-        <p>Please, insert this code in the password recovery page.</p>';
-        $cabeceras = 'From: '.$localSender . "\r\n" .
-            'Reply-To: '.$localSender . "\r\n" .
-            'MIME-Version: 1.0' . "\r\n" .
-            'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-    
-        mail($email, $titulo, $mensaje, $cabeceras);
+        $mail = new PHPMailer(true);
 
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                      
+            $mail->isSMTP();                                            
+            $mail->Host       = $localHost;                     
+            $mail->SMTPAuth   = true;                                   
+            $mail->Username   = $localUsername;                     
+            $mail->Password   = $localPassword;                              
+            $mail->SMTPSecure = $localSMTPSecure;             
+            $mail->Port       = $localPort;           
+            
+            //Recipients
+            $mail->setFrom($localUsername, 'HDDriveSupport');
+            $mail->addAddress($email, 'HDDriveUser');     //mail que recibira el correo
+
+            //Content
+            $mail->isHTML(true);                                  
+            $mail->Subject = 'Password change code';
+            $mail->Body    = '<p>You are about to change your password.</p>
+            <p>Your <b>verification code</b> is: <b>'.$verificationCode.'</b></p>
+            <p>Please, insert this code in the password recovery page.</p>';
+            $mail->AltBody = 'You are about to change your password. Your *verification code* is: > '.$verificationCode.' < Please, insert this code in the password recovery page.';
+
+            $mail->send();
+            
+        } catch (Exception $e) {                  
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
     }
 
     //Función que genera el código random de verificación
